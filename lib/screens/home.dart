0dc18088285018f89f6f55d1e671d127d4e5a0f4
Mahmoud Ashour <mashour365@gmail.com>
2021-05-10 17:52:30 +0200

    Bug fices

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,34 +9,48 @@ class HomeWidget extends StatefulWidget {
 
 class _HomeWidgetState extends State<HomeWidget> {
   int _selectedIndex = 0;
+  double _opacity = 1;
+  final _navigationList = [
+    ButtonData(Text('Contest'), FaIcon(FontAwesomeIcons.question)),
+    ButtonData(Text('Practice'), FaIcon(FontAwesomeIcons.question)),
+    ButtonData(Text('Share'), FaIcon(FontAwesomeIcons.question)),
+    ButtonData(Text('Help'), FaIcon(FontAwesomeIcons.question))
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                colors: [
-                  Color(0xff2a1434),
-                  Color(0xff160d20),
-                  Color(0xff142332)
-                ],
+          GestureDetector(
+            onTap: () => _opacity == 0 ? _opacity = 1 : _opacity = 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  colors: [
+                    Color(0xff2a1434),
+                    Color(0xff160d20),
+                    Color(0xff142332)
+                  ],
+                ),
               ),
             ),
           ),
-          GradientArcLight(
-            currentIndex: _selectedIndex,
-            onItemPressed: (i) {
-              setState(() => _selectedIndex = i);
-            },
-            buttons: [
-              ButtonData(Text('Contest'), FaIcon(FontAwesomeIcons.question)),
-              ButtonData(Text('Practice'), FaIcon(FontAwesomeIcons.question)),
-              ButtonData(Text('Share'), FaIcon(FontAwesomeIcons.question)),
-              ButtonData(Text('Help'), FaIcon(FontAwesomeIcons.question))
-            ],
+          AnimatedOpacity(
+            duration: Duration(seconds: 1),
+            opacity: _opacity,
+            child: SafeArea(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: GradientArcLight(
+                  currentIndex: _selectedIndex,
+                  onItemPressed: (i) {
+                    setState(() => _selectedIndex = i);
+                  },
+                  buttons: _navigationList,
+                ),
+              ),
+            ),
           ),
         ],
       ),
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -10,28 +10,30 @@ class SplashWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     /// [todo] Fetch data and load login or home screen.
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      Future.delayed(
-        Duration(seconds: 2),
-        () async {
-          bool? isAuth = await Utils.provider(context, listen: false).isAuth();
-          print(isAuth);
-          return Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(
-                builder: (context) =>
-                    isAuth! ? HomeWidget() : AuthenticationWidget(Auth.SignIn),
-              ),
-              (Route<dynamic> route) => false);
-        },
-      );
-    });
+    WidgetsBinding.instance!.addPostFrameCallback(
+      (_) {
+        Future.delayed(
+          Duration(seconds: 2),
+          () async {
+            bool? isAuth =
+                await Utils.provider(context, listen: false).isAuth();
+            print(isAuth);
+            return Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                  builder: (context) => isAuth!
+                      ? HomeWidget()
+                      : AuthenticationWidget(Auth.SignIn),
+                ),
+                (Route<dynamic> route) => false);
+          },
+        );
+      },
+    );
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.white),
-          ),
+          Container(decoration: BoxDecoration(color: Colors.white)),
           Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
